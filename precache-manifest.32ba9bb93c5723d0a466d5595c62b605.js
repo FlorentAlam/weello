352cdd161dafb3e2b67346e38b76e032956421ab
Florent Alamachere <florentalamachere@yahoo.fr>
2019-07-22 13:38:51 +0200

    Updates

---
+++ precache-manifest.32ba9bb93c5723d0a466d5595c62b605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a731d17c77318166796b1c2a21c6ade7",
+    "url": "/weello/index.html"
+  },
+  {
+    "revision": "a259c8f93d39d444d68b",
+    "url": "/weello/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "599cc49a1cdb36d858cf",
+    "url": "/weello/static/css/main.714f2f8c.chunk.css"
+  },
+  {
+    "revision": "a259c8f93d39d444d68b",
+    "url": "/weello/static/js/2.bacc56e9.chunk.js"
+  },
+  {
+    "revision": "599cc49a1cdb36d858cf",
+    "url": "/weello/static/js/main.e7478692.chunk.js"
+  },
+  {
+    "revision": "dda30f340a5651918455",
+    "url": "/weello/static/js/runtime~main.5065132c.js"
+  }
+]);
